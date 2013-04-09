0cab4cb51c83c35ac11cea1e125db2c706d50ea3
Admin <Admin@Admins-Mac.local>
2013-04-09 10:42:27 +0300

    Added initial project

---
+++ DiscogsAPI/DiscogsAPI/DiscogsAPI.h
@@ -0,0 +1,13 @@
+//
+//  DiscogsAPI.h
+//  DiscogsAPI
+//
+//  Created by Admin on 4/9/13.
+//  Copyright (c) 2013 bbonch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DiscogsAPI : NSObject
+
+@end
